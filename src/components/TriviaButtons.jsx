368b20022a5d8388c3b1be6880f4ff910473721a
TMZSeif <tmzseif.tamer@gmail.com>
2026-07-22 14:19:41 +0300

    added fetch random wikipedia page functionality

--- src/components/TriviaButtons.jsx
+++ src/components/TriviaButtons.jsx
@@ -1,7 +1,9 @@
+import WikipediaButton from "./WikipediaButton"
+
 export default function TriviaButtons() {
 	return (
 		<div className="d-flex flex-column button-layout">
-			<button type="button" className="btn-ui">Surprise me</button>
+			<WikipediaButton />
 			<button type="button" className="btn-ui">Give me a word</button>
 			<button type="button" className="btn-ui">Atrocities</button>
 		</div>
---
+++ src/components/WikipediaButton.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+export default function WikipediaButton() {
+	const [wikiText, setWikiText] = useState("")
+
+	const fetchRandomPage = async (event) => {
+		let response = await fetch("https://en.wikipedia.org/api/rest_v1/page/random/summary")
+		let data = await response.json()
+
+		const title = data["titles"]["canonical"]
+		response = await fetch(`https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=${title}&rvslots=*&rvprop=content&formatversion=2&format=json&origin=*`)
+		data = await response.json()
+		const wikiText = data["query"]["pages"][0]["revisions"][0]["slots"]["main"]["content"]
+		setWikiText(wikiText)
+	}
+
+	return (
+		<button type="button" onClick={fetchRandomPage} className="btn-ui">Surprise me</button>
+	)
+}
