007538c7004c31adaf967fa481faa5d309fce02f
TMZSeif <tmzseif.tamer@gmail.com>
2026-07-22 17:03:18 +0300

    added wikitext to html functionality

--- src/pages/WikipediaPage.jsx
+++ src/pages/WikipediaPage.jsx
@@ -1,6 +1,6 @@
 import { useLocation } from 'react-router-dom'
-import style from '../wikipedia.module.css'
 import { useEffect, useState } from 'react'
+import DOMPurify from 'dompurify'
 
 export default function WikipediaPage() {
 	const location = useLocation()
@@ -11,15 +11,27 @@ export default function WikipediaPage() {
 	useEffect(() => {
 		async function parseText() {
 			console.log(wikitext)
-			const response = await fetch(`https://en.wikipedia.org/w/api.php?action=parse&page=${title}&format=json&origin=*`)
+			const response = await fetch(`https://en.wikipedia.org/w/api.php?action=parse&title=${title}&contentmodel=wikitext&disablelimitreport=true&format=json&origin=*`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded"
+				},
+				body: new URLSearchParams({
+					text: wikitext
+				})
+			})
 			const data = await response.json()
-			console.log(data)
+			let htmltext = data["parse"]["text"]["*"].replaceAll('\\"', "'")
+			htmltext = DOMPurify.sanitize(htmltext)
+			setHtmltext(htmltext)
+			console.log(htmltext)
 		}
 		parseText()
 	}, [])
 
 
 	return (
-		<div></div>
+		<div dangerouslySetInnerHTML={{ __html: htmltext}}>
+		</div>
 	)
 }
