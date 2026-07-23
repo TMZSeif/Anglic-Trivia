548b19a447c898f247e4de6a998363c848f5e1b5
TMZSeif <tmzseif.tamer@gmail.com>
2026-07-23 19:16:50 +0300

    added wikipedia css styling

--- src/components/WikipediaButton.jsx
+++ src/components/WikipediaButton.jsx
@@ -12,6 +12,7 @@ export default function WikipediaButton({ handleLoading }) {
 		const title = data["titles"]["canonical"]
 		response = await fetch(`https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=${title}&rvslots=*&rvprop=content&formatversion=2&format=json&origin=*`)
 		data = await response.json()
+		console.log(data)
 		const wikiText = data["query"]["pages"][0]["revisions"][0]["slots"]["main"]["content"]
 		handleLoading(false)
 		navigate("/wikipedia", { state: {wikitext: wikiText, title: title}})
--- src/pages/WikipediaPage.jsx
+++ src/pages/WikipediaPage.jsx
@@ -5,13 +5,14 @@ import DOMPurify from 'dompurify'
 export default function WikipediaPage() {
 	const location = useLocation()
 	const wikitext = location.state?.wikitext
-	const title = location.state?.title
+	const titleLink = location.state?.title
 	const [htmltext, setHtmltext] = useState("")
+	const [title, setTitle] = useState(titleLink)
 
 	useEffect(() => {
 		async function parseText() {
 			console.log(wikitext)
-			const response = await fetch(`https://en.wikipedia.org/w/api.php?action=parse&title=${title}&contentmodel=wikitext&disablelimitreport=true&format=json&origin=*`, {
+			const response = await fetch(`https://en.wikipedia.org/w/api.php?action=parse&title=${titleLink}&contentmodel=wikitext&disableeditsection=true&disablelimitreport=true&format=json&origin=*`, {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/x-www-form-urlencoded"
@@ -21,17 +22,36 @@ export default function WikipediaPage() {
 				})
 			})
 			const data = await response.json()
+			console.log(data)
 			let htmltext = data["parse"]["text"]["*"].replaceAll('\\"', "'")
 			htmltext = DOMPurify.sanitize(htmltext)
 			setHtmltext(htmltext)
-			console.log(htmltext)
 		}
 		parseText()
 	}, [])
 
 
 	return (
-		<div dangerouslySetInnerHTML={{ __html: htmltext}}>
+		<div className='mw-page-container'>
+			<div className='my-content-container'>
+				<main id='content' className='mw-body'>
+					<header className='mw-body-header vector-page-titlebar no-font-mode-scale'>
+						<h1 id='firstHeading' className='firstHeading mw-first-heading'>
+							<span lang='en' dir='ltr'>
+								<span className='mw-page-title-main'>{title.replace("_", " ")}</span>
+							</span>
+						</h1>
+					</header>
+					<div id='bodyContent' className='vector-body ve-init-mw-desktopArticleTarget-targetContainer' aria-labelledby='firstHeading' data-me-ve-target-container>
+						<div className='vector-body-before-content'>
+							<div id='siteSub' className='noprint'>From Wikipedia, the free encyclopedia</div>
+						</div>
+						<div id='mw-content-text' className='mw-body-content'>
+							<div dangerouslySetInnerHTML={{__html: htmltext}}></div>
+						</div>
+					</div>
+				</main>
+			</div>
 		</div>
 	)
 }
