213f330a77a93b6ea03580b6c4740a4a79b8a8aa
TMZSeif <tmzseif.tamer@gmail.com>
2026-08-08 04:44:03 +0300

    added base path

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import WikipediaPage from './pages/WikipediaPage'
 
 function App() {
 
 	return (
-		<>
+		<BrowserRouter basename='/Anglic-Trivia'>
 			<Routes>
 				<Route path='/' element={<HomePage />} />
 				<Route path='/wikipedia' element={<WikipediaPage />} />
 			</Routes>
-		</>
+		</BrowserRouter>
 	)
 }
 
